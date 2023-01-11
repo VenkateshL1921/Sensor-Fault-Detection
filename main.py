2ed97f7268e3255c8b23aef330bf93ee27256fc1
venkatesh <venkyslotlikar19@gmail.com>
2023-01-11 16:13:42 +0530

    updated

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from Sensor.configuration.mongo_db_connection import MongoDBClient
 from Sensor.exception import SensorException
 from Sensor.logger import logging
 from Sensor.entity.config_entity import TrainingPipelineConfig, DataIngestionConfig
@@ -14,6 +13,11 @@ from Sensor.ml.model.estimator import ModelResolver, TargetValueMapping
 from Sensor.utils.main_utils import load_object
 from Sensor.constant.training_pipeline import SAVED_MODEL_DIR
 from fastapi.middleware.cors import CORSMiddleware
+import pandas as pd
+from Sensor.constant.training_pipeline import TARGET_COLUMN
+import pickle
+from Sensor.utils.main_utils import read_yaml_file
+from Sensor.constant.training_pipeline import SCHEMA_FILE_PATH
 
 app = FastAPI()
 origins = ["*"]
@@ -46,23 +50,28 @@ async def train_route():
 @app.get("/predict")
 async def predict_route():
     try:
-        # get data from user csv file
-        # conver csv file to dataframe
-
-        df = None
+        schema_config = read_yaml_file(SCHEMA_FILE_PATH)
+        filename = "C:/Users/venkatesh lotlikar/Desktop/Portfololio Projects/Sensor-Fault-Detection/artifact/01_11_2023_10_46_41/data_transformation/transformed_object/preprocessing.pkl"
+        transformer = pickle.load(open(filename, "rb"))
+        df = pd.read_csv(
+            "C:/Users/venkatesh lotlikar/Desktop/Portfololio Projects/Sensor-Fault-Detection/artifact/01_11_2023_10_46_41/data_ingestion/feature_store/sensor.csv")
+        df = pd.DataFrame(df)
+        df1 = df.head(1)
+        df2 = df1.drop(schema_config["drop_columns"], axis=1)
         model_resolver = ModelResolver(model_dir=SAVED_MODEL_DIR)
         if not model_resolver.is_model_exists():
             return Response("Model is not available")
 
         best_model_path = model_resolver.get_best_model_path()
         model = load_object(file_path=best_model_path)
-        y_pred = model.predict(df)
-        df['predicted_column'] = y_pred
-        df['predicted_column'].replace(
+        df1_features = df2.drop(TARGET_COLUMN, axis=1, inplace=True)
+        df_transformed = transformer.fit_transform(df1_features)
+        y_pred = model.predict(df_transformed)
+        df1_features['predicted_column'] = y_pred
+        df1_features['predicted_column'].replace(
             TargetValueMapping().reverse_mapping(), inplace=True)
 
-        
-
+        return {"predicted": y_pred}
     except Exception as e:
         raise Response(f"Error Occured! {e}")
 
@@ -78,4 +87,3 @@ def main():
 
 if __name__ == "__main__":
     app_run(app, host=APP_HOST, port=APP_PORT)
-   
